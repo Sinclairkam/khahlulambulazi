9b6a08ab0b8df18d06ae6e829396da35be441d16
Sinclairkam <163323684+Sinclairkam@users.noreply.github.com>
2026-07-04 16:22:07 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/khahlulambulazi/', // <-- Keeps your paths correct on GitHub Pages
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
